46e68f6fafed5d6f7f0797818a1c4a6dfb40a404
chmuthu <117966832+chmuthu@users.noreply.github.com>
2022-12-12 09:10:13 -0800

    Update monitoringSetup.sh

--- chaos-experiments/monitoring/monitoringSetup.sh
+++ chaos-experiments/monitoring/monitoringSetup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-export NODE_ROLE_NAME=$1
+export APPS_NODE_ROLE_NAME=$1
+export PF_NODE_ROLE_NAME=$2
 
 set +x
 echo "================"
 echo "--Metrics Server Installation==> START--"
 echo "================"
 set -x
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl apply -f metrics-server.yaml
 sleep 10
 #Check
 
@@ -30,28 +31,14 @@ kubectl create ns grafana
 
 sleep 5
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
+kubectl apply -f kiali.yaml
+kubectl apply -f prometheus.yaml
+kubectl apply -f grafana.yaml
 
 sleep 5
 
-helm install prometheus prometheus-community/prometheus --namespace prometheus --set alertmanager.persistentVolume.storageClass="gp2" --set server.persistentVolume.storageClass="gp2"
-
-sleep 10
-
-kubectl get all -n prometheus
-
-sleep 5
-
-helm install grafana grafana/grafana --namespace grafana --set persistence.storageClassName="gp2" --set persistence.enabled=true --set adminPassword='EKS' --values grafana.yaml --set service.type=LoadBalancer
-
-sleep 10
-
-kubectl get all -n grafana
-
-sleep 5
-
-echo "Grafana Endpoint: " kubectl get svc -n grafana
+kubectl patch svc grafana -p '{"spec": {"type": "LoadBalancer"}}' -n istio-system
+kubectl patch svc kiali -p '{"spec": {"type": "LoadBalancer"}}' -n istio-system
 
 sleep 5
 
@@ -79,15 +66,20 @@ kubectl apply -f cwagent-configmap.yaml
 
 sleep 5
 
-kubectl apply -f https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/cwagent/cwagent-daemonset.yaml
+kubectl apply -f cwagent-daemonset.yaml
 
 sleep 10
 
 #Attach IAM policy to Worker Node Role
-aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy --role-name $NODE_ROLE_NAME
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy --role-name $APPS_NODE_ROLE_NAME
+aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy --role-name $PF_NODE_ROLE_NAME
 
 kubectl get pods -n amazon-cloudwatch
 
+sleep 5
+
+kubectl get svc -n istio-system
+sleep 5
 set +x
 echo "================"
 echo "--CloudWatch ContainerInsights Installation ==> END--"
